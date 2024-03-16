7788d40e39a8eb17383af9b18cb637b1ae4a4f52
Aurelijus Lukšas <aurelijus.luksas@mif.stud.vu.lt>
2024-03-16 15:57:13 +0200

    Initial commit.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module main.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens main.guiprogram to javafx.fxml;
+    exports main.guiprogram;
+}
